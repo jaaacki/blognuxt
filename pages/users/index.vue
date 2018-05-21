17538d898c3b152ecf7430555c07b74cc22e33bb
eugenekyj <eugene@ada.asia>
2018-05-21 18:33:34 +0800

    Added the button for user id

---
+++ pages/users/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h1>The users view</h1>
+    <nuxt-link to="/">Home</nuxt-link>
+
+<input type="text" v-model="UserId">
+<button @click="onLoadUser">Load User</button>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return
+        UserId: ''
+    },
+    methods: {
+        onLoadUser() {
+           this.$router.push('/users/' + this.UserId)
+        }
+    }
+
+}
+</script>
