060cd24eb5af2a25fec065b5e5107619f2a2abe4
jaaacki <jacki.da@gmail.com>
2018-05-22 00:45:40 +0800

    removed everything except new folders

---
+++ pages/users/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h1>Users</h1>      
+      <input type="text" v-model="userId"></input>
+      <button @click="onLoadUser">Load User</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            userId:''
+        }
+    },
+    methods: {
+        onLoadUser() {
+            this.$router.push('/users/' + this.userId)
+
+        }
+    }
+}
+</script>
