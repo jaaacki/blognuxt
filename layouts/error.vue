b2091e3d074f8b32a8e196d9401bb43f45ca0aa3
Jaaacki <jacki.da@gmail.com>
2018-05-21 23:19:25 +0800

    remove all files, redoing the project files

--- layouts/error.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>You have an error!</h1>
-</template>
--- pages/users/_id/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1>A single user, with ID: {{ $route.params.id }}</h1>
-</template>
-
-<script>
-export default {
-    layout: 'users'
-}
-</script>
